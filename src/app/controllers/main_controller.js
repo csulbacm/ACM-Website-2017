16b41ea77c55d49a42736247052374606038cc6e
Rich Cherngchaosil <richer_po@hotmail.com>
2017-07-16 15:27:02 -0700

    Link event and beachhacks to their page

--- src/app/controllers/main_controller.js
+++ src/app/controllers/main_controller.js
@@ -3,24 +3,50 @@ acm_app.controller("main_controller", function($scope){
 	this.tab1 = {
 		EVENT: {
 			name: "Event",
-			url: "event"
+			state: "null",
+			url: "https://www.facebook.com/csulbacm/"
 		},
 		ABOUT: {
 			name: "About & Board",
-			url: "about-board"
+			state: "about-board"
 		}
 	};
 
 
 	this.home = {
 			name: "Home",
-			url: "home"
+			state: "home"
 	}
 
 	this.tab2 = {
 		CONTACT: {
 			name: "Contact",
-			url: "contact"
+			state: "contact"
+		},
+		BEACHHACK: {
+			name: "Beachhack",
+			state: "null",
+			url: "http://beachhacks.com/"
 		}
 	};
+
+
+	this.getNavRoute = function(_tab){
+		if(_tab.state == "null") {
+			return _tab.url;
+		} else {
+			return "";
+		}
+	}
+
+	this.checkLink = function(_tab){
+		console.log(_tab);
+		if(_tab.state == "null" || _tab.state == ""){
+			return "_blank";
+		} else {
+			return "";
+		}
+
+
+	}
 });
