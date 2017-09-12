5ae8754a6c01a2a81b06d0f708ec4dca1a96eab0
RichCherng <richcherng@gmail.com>
2017-09-11 21:21:41 -0700

    deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,32 @@
+#!/bin/sh
+# @author: Alexandre Plennevaus
+# @description: MIRROR DISTANT FOLDER TO LOCAL FOLDER VIA FTP
+#
+# FTP LOGIN
 
-lftp -f "
-open $host
-user $user
-$password
-lcd.
-bye
-"
+HOST=$host
+USER=$user
+PASSWORD=$password
+
+#DISTANT DIRECTORY
+REMOTE_DIR='/'
+
+#LOCAL DIRECTORY
+LOCAL_DIR='.'
+
+# RUNTIME!
+echo
+echo "Starting download $REMOTE_DIR from $HOST to $LOCAL_DIR"
+date
+
+lftp -u "$USER","$PASSWORD" $HOST <<EOF
+lcd .
+pwd
+ls
+exit
+EOF
+
+echo
+echo "Transfer finished"
+date
+
