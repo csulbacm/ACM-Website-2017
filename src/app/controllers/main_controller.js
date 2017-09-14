0f2e7cf2c150df6fc807bd03c72769e7f884f90f
Rich Cherngchaosil <richer_po@hotmail.com>
2017-09-13 23:13:43 -0700

    change nav-bar

--- src/app/controllers/main_controller.js
+++ src/app/controllers/main_controller.js
@@ -1,16 +1,16 @@
 acm_app.controller("main_controller", function($scope){
 
-	this.tab1 = {
-		EVENT: {
-			name: "Event",
-			state: "null",
-			url: "https://www.facebook.com/csulbacm/"
-		},
-		ABOUT: {
-			name: "About & Board",
-			state: "about-board"
-		}
-	};
+	// this.tab1 = {
+	// 	EVENT: {
+	// 		name: "Event",
+	// 		state: "null",
+	// 		url: "https://www.facebook.com/csulbacm/"
+	// 	},
+	// 	ABOUT: {
+	// 		name: "About & Board",
+	// 		state: "about-board"
+	// 	}
+	// };
 
 
 	this.home = {
@@ -18,13 +18,35 @@ acm_app.controller("main_controller", function($scope){
 			state: "home"
 	}
 
-	this.tab2 = {
-		/*
-		CONTACT: {
-			name: "Contact",
-			state: "contact"
+
+	// this.tab2 = {
+	// 	/*
+	// 	CONTACT: {
+	// 		name: "Contact",
+	// 		state: "contact"
+	// 	},
+	// 	*/
+	// 	HUB: {
+	// 		name: "Activities Hub",
+	// 		state: "hub"
+	// 	},
+	// 	BEACHHACK: {
+	// 		name: "Beachhack",
+	// 		state: "null",
+	// 		url: "http://beachhacks.com/"
+	// 	}
+	// };
+
+	this.tab = {
+		ABOUT: {
+			name: "About",
+			state: "about-board"
 		},
-		*/
+		EVENT: {
+			name: "Event",
+			state: "null",
+			url: "https://www.facebook.com/csulbacm/"
+		},
 		HUB: {
 			name: "Activities Hub",
 			state: "hub"
@@ -34,11 +56,14 @@ acm_app.controller("main_controller", function($scope){
 			state: "null",
 			url: "http://beachhacks.com/"
 		}
-	};
+	}
+
+	this.currentTab = "/";
 
 
 	this.getNavRoute = function(_tab){
 		if(_tab.state == "null") {
+			this.currentTab = _tab.url
 			return _tab.url;
 		} else {
 			return "";
