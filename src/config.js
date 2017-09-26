b3714e7442b2b2aff6af99e3f5c2f50f08cadb00
Rich Cherngchaosil <richer_po@hotmail.com>
2017-09-26 00:02:21 -0700

    Fixed Typo, Issue #12

--- src/config.js
+++ src/config.js
@@ -10,11 +10,11 @@ const config = {
 			header: "Our Goal",
 			paragraphs: [
 			"Our goal here at ACM is to advance the science and application of information technology by providing students with real world information through guest speakers, workshops, seminars, projects, and other activities. Information about job trends, internships, and scholarships is always updated to help students take a stand in their future career.",
-			"We also work to strengthen the bond between the studetns, the faculty, and the Computer Engineering and Computer Science (CECS) department. A stronger, tighter relationship students and their instructors engerders a friendly, energetic, close-knit and secure learning atmosphere.",
+			"We also work to strengthen the bond between the students, the faculty, and the Computer Engineering and Computer Science (CECS) department. A stronger, tighter relationship students and their instructors engenders a friendly, energetic, close-knit and secure learning atmosphere.",
 			"Moreover, we provide services back to the community by tutoring, volunteering, and spreading our knowledge about the information technology to others."]
 		},{
 			header: "More about acm@thebeach",
-			paragraphs: ["The Association for Computing Machinery is an active branch of the globe's largest computer science society. Here at the Long Beach chapter, we blieve in the principles of Gladwell's book, Outliers: The Story of Success. We commit our time and energy to producing, engaging in, and ultimately mastering the technologies of the computing age."]
+			paragraphs: ["The Association for Computing Machinery is an active branch of the globe's largest computer science society. Here at the Long Beach chapter, we believe in the principles of Gladwell's book, Outliers: The Story of Success. We commit our time and energy to producing, engaging in, and ultimately mastering the technologies of the computing age."]
 		}],
 	} ,
 
